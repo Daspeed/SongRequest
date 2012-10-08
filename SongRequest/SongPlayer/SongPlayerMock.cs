85cc49916635a7e87e7e02c5906d65479f8a108e
Henk Westhuis <henk_westhuis@hotmail.com>
2012-10-08 19:05:49 +0200

    Added SongLibrary

--- SongRequest/SongPlayer/SongPlayerMock.cs
+++ SongRequest/SongPlayer/SongPlayerMock.cs
@@ -6,25 +6,25 @@ namespace SongRequest
 {
 	public class SongPlayerMock : ISongplayer
 	{
-		private List<Song> _songs;
+		private SongLibrary songLibrary;
+		
 		private List<Song> _queue;
 		private Song _currentSong;
 		private DateTime _currentSongStart;
-		private Random random = new Random();
 
 		public SongPlayerMock ()
 		{
-			_songs = new List<Song> {
-							            new Song(){ Artist="4 Strings", Name="Summer Sun", Duration = TimeSpan.FromSeconds(320), FileName="4.mp3"},
-							            new Song(){ Artist="Adele", Name="Set Fire To The Rain", Duration = TimeSpan.FromSeconds(410), FileName="A.mp3"},
-							            new Song(){ Artist="Silverblue", Name="Step Back", Duration = TimeSpan.FromSeconds(340), FileName="S.mp3"},
-							            new Song(){ Artist="Ilse DeLange", Name="I'm not so tough", Duration = TimeSpan.FromSeconds(523), FileName="I.mp3"},
-							            new Song(){ Artist="Coldplay", Name="Clocks", Duration = TimeSpan.FromSeconds(333), FileName="C.mp3"},
-							            new Song(){ Artist="Queen", Name="Bohemian Rapsody", Duration = TimeSpan.FromSeconds(621), FileName="Q.mp3"},
-										new Song(){ Artist="The prodigy", Name="Smack My Bitch Up", Duration = TimeSpan.FromSeconds(536), FileName="Q.mp3"},
-							        };
+			songLibrary = new SongLibrary();
 			
-			_queue = new List<Song>(_songs.Take(3));	
+			songLibrary.AddSong(new Song(){ Artist="4 Strings", Name="Summer Sun", Duration = TimeSpan.FromSeconds(6), FileName="4.mp3"});
+			songLibrary.AddSong(new Song(){ Artist="Adele", Name="Set Fire To The Rain", Duration = TimeSpan.FromSeconds(41), FileName="A.mp3"});
+			songLibrary.AddSong(new Song(){ Artist="Silverblue", Name="Step Back", Duration = TimeSpan.FromSeconds(34), FileName="S.mp3"});
+			songLibrary.AddSong(new Song(){ Artist="Ilse DeLange", Name="I'm not so tough", Duration = TimeSpan.FromSeconds(52), FileName="I.mp3"});
+			songLibrary.AddSong(new Song(){ Artist="Coldplay", Name="Clocks", Duration = TimeSpan.FromSeconds(33), FileName="C.mp3"});
+			songLibrary.AddSong(new Song(){ Artist="Queen", Name="Bohemian Rapsody", Duration = TimeSpan.FromSeconds(21), FileName="Q.mp3"});
+			songLibrary.AddSong(new Song(){ Artist="The prodigy", Name="Smack My Bitch Up", Duration = TimeSpan.FromSeconds(36), FileName="Q.mp3"});
+			
+			_queue = new List<Song>(songLibrary.GetSongs(string.Empty, 0, 3));
 		}
 		
 		public PlayerStatus PlayerStatus 
@@ -33,7 +33,7 @@ namespace SongRequest
 			{
 				if ( _currentSong == null ||
 				     _currentSong.Duration == null ||
-				    (_currentSong.Duration.Value - (DateTime.Now - _currentSongStart)).TotalSeconds == 0)
+				    (_currentSong.Duration.Value - (DateTime.Now - _currentSongStart)).TotalSeconds <= 0)
 				{
 					if (_queue.Count > 0)
 					{					
@@ -44,7 +44,7 @@ namespace SongRequest
 					} else
 					{
 						//Take random song
-						_currentSong = _songs[random.Next(_songs.Count - 1)];
+						_currentSong = songLibrary.GetRandomSong();
 					}
 					
 					_currentSongStart = DateTime.Now;
@@ -62,7 +62,7 @@ namespace SongRequest
 		{
 			get
 			{
-				return _songs;
+				return songLibrary.GetSongs(string.Empty, 0, 100);
 			}
 		}
 		
