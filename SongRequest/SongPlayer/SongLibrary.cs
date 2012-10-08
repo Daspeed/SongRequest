0eed4e769925e0ad78e97d9df3cda037782043a7
Henk Westhuis <henk_westhuis@hotmail.com>
2012-10-08 12:06:13 -0700

    Added error handling and more stable song change

--- SongRequest/SongPlayer/SongLibrary.cs
+++ SongRequest/SongPlayer/SongLibrary.cs
@@ -6,51 +6,61 @@ using Id3;
 
 namespace SongRequest
 {
-	public class SongLibrary
-	{
-		private Random random = new Random();
-		private List<Song> _songs;
-		
-		public SongLibrary ()
-		{
-			_songs = new List<Song>();
-		}
-		
-		public void ScanSongs(string directory)
-		{
-			foreach(string fileName in Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories))
-			{
-				//Do some magic...
-                Song song = new Song();
-                song.FileName = fileName;
+    public class SongLibrary
+    {
+        private Random random = new Random();
+        private List<Song> _songs;
 
-                using (var mp3 = new Mp3File(fileName))
+        public SongLibrary()
+        {
+            _songs = new List<Song>();
+        }
+
+        public void ScanSongs(string directory)
+        {
+            if (Directory.Exists("c:\\music"))
+                foreach (string fileName in Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories))
                 {
-                    Id3Tag tag = mp3.GetTag(Id3TagFamily.FileStartTag);
-                    song.Name = tag.Title.Value;
-                    song.Artist = tag.Artists.Value;
-                    song.Duration = mp3.Audio.Duration;
+                    try
+                    {
+                        //Do some magic...
+                        Song song = new Song();
+                        song.FileName = fileName;
+
+                        using (var mp3 = new Mp3File(fileName))
+                        {
+                            Id3Tag tag = mp3.GetTag(Id3TagFamily.FileStartTag);
+                            song.Name = tag == null || tag.Title == null ? "unknown" : tag.Title.Value;
+                            song.Artist = tag == null || tag.Artists == null ? "unknown" : tag.Artists.Value;
+                            song.Duration = mp3.Audio.Duration;
+                        }
+
+                        _songs.Add(song);
+                    }
+                    catch
+                    {
+                    }
+
                 }
+        }
 
-                _songs.Add(song);
+        public void AddSong(Song song)
+        {
+            _songs.Add(song);
+        }
 
-			}
-		}		
-		
-		public void AddSong(Song song)
-		{
-			_songs.Add(song);
-		}
-		
-		public IEnumerable<Song> GetSongs(string filter, int top, int count)
-		{
-			return _songs.Where(s => s.FileName.ToLower().Contains(filter.ToLower())).Skip(top).Take(count);
-		}
-		
-		public Song GetRandomSong()
-		{
-			return _songs[random.Next(_songs.Count() - 1)];
-		}
-	}
+        public IEnumerable<Song> GetSongs(string filter, int top, int count)
+        {
+            return _songs.Where(s => s.FileName.ToLower().Contains(filter.ToLower())).Skip(top).Take(count);
+        }
+
+        public Song GetRandomSong()
+        {
+            if (_songs.Count == 0)
+                return null;
+
+            return _songs[random.Next(_songs.Count())];
+        }
+    }
 }
 
