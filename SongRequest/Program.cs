5603adac121950b43f55bd01a87479d160a8c16f
Erik Peters <erikpeters@outlook.com>
2013-09-02 13:57:04 +0200

    More changes for responsive html

--- SongRequest/Program.cs
+++ SongRequest/Program.cs
@@ -5,162 +5,161 @@ using System.Net;
 
 namespace SongRequest
 {
-    class Program
-    {
-        internal volatile static bool _running = true;
-        private static int port;
+	class Program
+	{
+		internal volatile static bool _running = true;
+		private static int port;
 
-        static string prefix = "http://*:9669/";
+		static string prefix = "http://*:8765/";
 
 
-        static void Main(string[] args)
-        {
-            try
-            {
-                Run();
-            }
-            catch (HttpListenerException ex)
-            {
-                if (ex.ErrorCode == 5)
-                {
-                    string username = Environment.GetEnvironmentVariable("USERNAME");
-                    string userdomain = Environment.GetEnvironmentVariable("USERDOMAIN");
+		static void Main(string[] args)
+		{
+			try
+			{
+				Run();
+			}
+			catch (HttpListenerException ex)
+			{
+				if (ex.ErrorCode == 5)
+				{
+					string username = Environment.GetEnvironmentVariable("USERNAME");
+					string userdomain = Environment.GetEnvironmentVariable("USERDOMAIN");
 
-                    Console.SetCursorPosition(0, Console.WindowHeight - 10);
-                    Console.WriteLine("You need to run the following command (as admin):");
-                    Console.WriteLine("  netsh http add urlacl url={0} user={1}\\{2} listen=yes",
-                        prefix, userdomain, username);
-                    Console.SetCursorPosition(0, 0);
-                }
-                else
-                {
-                    Console.WriteLine(ex);
-                    Console.ReadLine();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                Console.ReadLine();
-            }
-        }
+					Console.SetCursorPosition(0, Console.WindowHeight - 10);
+					Console.WriteLine("You need to run the following command (as admin):");
+					Console.WriteLine("  netsh http add urlacl url={0} user={1}\\{2} listen=yes", prefix, userdomain, username);
+					Console.SetCursorPosition(0, 0);
+				}
+				else
+				{
+					Console.WriteLine(ex);
+					Console.ReadLine();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				Console.ReadLine();
+			}
+		}
 
-        private static void Run()
-        {
-            Console.Clear();
-            DrawArt();
+		private static void Run()
+		{
+			Console.Clear();
+			DrawArt();
 
-            using (HttpListener listener = new HttpListener())
-            {
-                if (!int.TryParse(SongPlayerFactory.GetConfigFile().GetValue("server.port"), out port))
-                    port = 8765;
+			using (HttpListener listener = new HttpListener())
+			{
+				if (!int.TryParse(SongPlayerFactory.GetConfigFile().GetValue("server.port"), out port))
+					port = 8765;
 
-                prefix = string.Format("http://*:{0}/", port);
+				prefix = string.Format("http://*:{0}/", port);
 
-                DrawProgramStatus();
+				DrawProgramStatus();
 
-                string stringVolume = SongPlayerFactory.GetConfigFile().GetValue("player.startupvolume");
-                int volume;
-                if (!int.TryParse(stringVolume, out volume))
-                    volume = 50;
+				string stringVolume = SongPlayerFactory.GetConfigFile().GetValue("player.startupvolume");
+				int volume;
+				if (!int.TryParse(stringVolume, out volume))
+					volume = 50;
 
-                SongPlayerFactory.GetSongPlayer().Volume = volume;
-                SongPlayerFactory.GetSongPlayer().LibraryStatusChanged += new StatusChangedEventHandler(Program_LibraryStatusChanged);
-                SongPlayerFactory.GetSongPlayer().PlayerStatusChanged += new StatusChangedEventHandler(Program_PlayerStatusChanged);
+				SongPlayerFactory.GetSongPlayer().Volume = volume;
+				SongPlayerFactory.GetSongPlayer().LibraryStatusChanged += new StatusChangedEventHandler(Program_LibraryStatusChanged);
+				SongPlayerFactory.GetSongPlayer().PlayerStatusChanged += new StatusChangedEventHandler(Program_PlayerStatusChanged);
 
-                listener.Prefixes.Add(prefix);
+				listener.Prefixes.Add(prefix);
 
-                listener.Start();
+				listener.Start();
 
-                while (_running)
-                {
-                    HttpListenerContext context = listener.GetContext();
-                    Stopwatch watch = Stopwatch.StartNew();
+				while (_running)
+				{
+					HttpListenerContext context = listener.GetContext();
+					Stopwatch watch = Stopwatch.StartNew();
 
-                    Program_LastRequestChanged(string.Format("{0} - {1}\t{2}", DateTime.Now.ToString("HH:mm:ss"), context.Request.UserHostAddress, context.Request.RawUrl));
+					Program_LastRequestChanged(string.Format("{0} - {1}\t{2}", DateTime.Now.ToString("HH:mm:ss"), context.Request.UserHostAddress, context.Request.RawUrl));
 
-                    try
-                    {
-                        Dispatcher.ProcessRequest(context);
+					try
+					{
+						Dispatcher.ProcessRequest(context);
 
-                        watch.Stop();
-                    }
-                    catch (Exception ex)
-                    {
-                        context.Response.StatusCode = 500;
+						watch.Stop();
+					}
+					catch (Exception ex)
+					{
+						context.Response.StatusCode = 500;
 
-                        using (var writer = new StreamWriter(context.Response.OutputStream))
-                            writer.Write(ex.ToString());
-                    }
-                }
-            }
-        }
+						using (var writer = new StreamWriter(context.Response.OutputStream))
+							writer.Write(ex.ToString());
+					}
+				}
+			}
+		}
 
-        static object consoleLock = new object();
+		static object consoleLock = new object();
 
-        static void Program_LibraryStatusChanged(string status)
-        {
-            lock (consoleLock)
-            {
-                Console.SetCursorPosition(0, 2);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, 2);
-                Console.Write("Library: {0}", SongPlayerFactory.GetConfigFile().GetValue("library.path"));
-                Console.SetCursorPosition(0, 3);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, 3);
-                Console.Write(status.Substring(0, Math.Min(status.Length, Console.WindowWidth)));
-            }
-        }
+		static void Program_LibraryStatusChanged(string status)
+		{
+			lock (consoleLock)
+			{
+				Console.SetCursorPosition(0, 2);
+				Console.Write(new string(' ', Console.WindowWidth));
+				Console.SetCursorPosition(0, 2);
+				Console.Write("Library: {0}", SongPlayerFactory.GetConfigFile().GetValue("library.path"));
+				Console.SetCursorPosition(0, 3);
+				Console.Write(new string(' ', Console.WindowWidth));
+				Console.SetCursorPosition(0, 3);
+				Console.Write(status.Substring(0, Math.Min(status.Length, Console.WindowWidth)));
+			}
+		}
 
-        static void Program_PlayerStatusChanged(string status)
-        {
-            lock (consoleLock)
-            {
-                Console.SetCursorPosition(0, 4);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, 4);
-                Console.Write(status.Substring(0, Math.Min(status.Length, Console.WindowWidth)));
-            }
-        }
+		static void Program_PlayerStatusChanged(string status)
+		{
+			lock (consoleLock)
+			{
+				Console.SetCursorPosition(0, 4);
+				Console.Write(new string(' ', Console.WindowWidth));
+				Console.SetCursorPosition(0, 4);
+				Console.Write(status.Substring(0, Math.Min(status.Length, Console.WindowWidth)));
+			}
+		}
 
-        static void Program_LastRequestChanged(string status)
-        {
-            lock (consoleLock)
-            {
-                Console.SetCursorPosition(0, 6);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, 6);
-                Console.Write(status.Substring(0, Math.Min(status.Length, Console.WindowWidth)));
-            }
-        }
+		static void Program_LastRequestChanged(string status)
+		{
+			lock (consoleLock)
+			{
+				Console.SetCursorPosition(0, 6);
+				Console.Write(new string(' ', Console.WindowWidth));
+				Console.SetCursorPosition(0, 6);
+				Console.Write(status.Substring(0, Math.Min(status.Length, Console.WindowWidth)));
+			}
+		}
 
-        static void DrawProgramStatus()
-        {
-            lock (consoleLock)
-            {
-                Console.SetCursorPosition(0, 1);
-                Console.Write("Listening on port: {0}", port);
-            }
-        }
+		static void DrawProgramStatus()
+		{
+			lock (consoleLock)
+			{
+				Console.SetCursorPosition(0, 1);
+				Console.Write("Listening on port: {0}", port);
+			}
+		}
 
-        static void DrawArt()
-        {
-            lock (consoleLock)
-            {
-                string white = new string(' ', Console.WindowWidth - 36);
-                Console.SetCursorPosition(0, Console.WindowHeight - 10);
-                Console.WriteLine(white + @"    ,");
-                Console.WriteLine(white + @"    |\        __");
-                Console.WriteLine(white + @"    | |      |--|             __");
-                Console.WriteLine(white + @"    |/       |  |            |~'");
-                Console.WriteLine(white + @"   /|_      () ()            |");
-                Console.WriteLine(white + @"  //| \             |\      ()");
-                Console.WriteLine(white + @" | \|_ |            | \ ");
-                Console.WriteLine(white + @"  \_|_/            ()  |");
-                Console.WriteLine(white + @"    |                  |");
-                Console.WriteLine(white + @"   @'                 ()");
-            }
-        }
-    }
+		static void DrawArt()
+		{
+			lock (consoleLock)
+			{
+				string white = new string(' ', Console.WindowWidth - 36);
+				Console.SetCursorPosition(0, Console.WindowHeight - 10);
+				Console.WriteLine(white + @"    ,");
+				Console.WriteLine(white + @"    |\        __");
+				Console.WriteLine(white + @"    | |      |--|             __");
+				Console.WriteLine(white + @"    |/       |  |            |~'");
+				Console.WriteLine(white + @"   /|_      () ()            |");
+				Console.WriteLine(white + @"  //| \             |\      ()");
+				Console.WriteLine(white + @" | \|_ |            | \ ");
+				Console.WriteLine(white + @"  \_|_/            ()  |");
+				Console.WriteLine(white + @"    |                  |");
+				Console.WriteLine(white + @"   @'                 ()");
+			}
+		}
+	}
 }
--- SongRequest/Static/SongRequest.js
+++ SongRequest/Static/SongRequest.js
@@ -5,235 +5,236 @@
             $compileProvider.urlSanitizationWhitelist(/^\s*file:/);
         })
         .controller('SongRequestController', function($scope, $http, $timeout) {
-            $scope.queue = [];
-            $scope.self = '';
-            $scope.playerStatus = {};
+	$scope.queue = [];
+	$scope.self = '';
+	$scope.playerStatus = {};
 
-            $scope.sortBy = 'artist';
-            $scope.ascending = true;
-            $scope.searchResult = [];
-            $scope.currentPage = 1;
-            $scope.filter = '';
-            $scope.pageCount = 0;
-            $scope.playlistStyle = {};
+	$scope.sortBy = 'artist';
+	$scope.ascending = true;
+	$scope.searchResult = [];
+	$scope.currentPage = 1;
+	$scope.filter = '';
+	$scope.pageCount = 0;
 
-            var convertNumberToTime = function (num) {
-                var minutes = parseInt(num / 60, 10);
-                var seconds = parseInt(num % 60, 10);
+	var convertNumberToTime = function (num) {
+		var minutes = parseInt(num / 60, 10);
+		var seconds = parseInt(num % 60, 10);
 
-                return String(minutes) + ((seconds < 10) ? ':0' : ':') + String(seconds);
-            };
-            $scope.convertNumberToTime = convertNumberToTime;
+		return String(minutes) + ((seconds < 10) ? ':0' : ':') + String(seconds);
+	};
+	$scope.convertNumberToTime = convertNumberToTime;
 
-            var fixer = function (num) {
-                return (num < 10 ? '0' : '') + String(num);
-            };
+	var fixer = function (num) {
+		return (num < 10 ? '0' : '') + String(num);
+	};
 
-            var digitize = function (arg) {
-                var allDigits = String(arg).replace(/\D/g, '');
-                if (allDigits && allDigits.length > 0)
-                    return parseInt(allDigits, 10);
-                else
-                    return 0;
-            };
+	var digitize = function (arg) {
+		var allDigits = String(arg).replace(/\D/g, '');
+		if (allDigits && allDigits.length > 0)
+			return parseInt(allDigits, 10);
+		else
+			return 0;
+	};
 
-            var enrichQueue = function (queue) {
-                if (!$scope.playerStatus || !$scope.playerStatus.RequestedSong)
-                    return [];
+	var enrichQueue = function (queue) {
+		if (!$scope.playerStatus || !$scope.playerStatus.RequestedSong)
+			return [];
 
-                var acc = Math.max(0, $scope.playerStatus.RequestedSong.Song.Duration - ($scope.playerStatus.Position || 0));
+		var acc = Math.max(0, $scope.playerStatus.RequestedSong.Song.Duration - ($scope.playerStatus.Position || 0));
 
-                angular.forEach(queue, function (value) {
-                    value.ETA = acc;
-                    acc += value.Song.Duration;
-                    value.Till = (function (baseDate) {
-                        baseDate.setSeconds(baseDate.getSeconds() + value.ETA);
-                        return fixer(baseDate.getHours()) + ':' + fixer(baseDate.getMinutes());
-                    })(new Date());
-                });
+		angular.forEach(queue, function (value) {
+			value.ETA = acc;
+			acc += value.Song.Duration;
+			value.Till = (function (baseDate) {
+				baseDate.setSeconds(baseDate.getSeconds() + value.ETA);
+				return fixer(baseDate.getHours()) + ':' + fixer(baseDate.getMinutes());
+			})(new Date());
+		});
 
-                return queue;
-            };
+		return queue;
+	};
 
-            var refreshQueue = function () {
-                $http({ method: 'GET', url: '/dynamic/queue' }).success(function (data) {
-                    $scope.queue = enrichQueue(data.Queue);
-                    $scope.self = data.Self;
-                    $scope.playerStatus = data.PlayerStatus;
-                });
-            };
+	var refreshQueue = function () {
+		$http({ method: 'GET', url: '/dynamic/queue' }).success(function (data) {
+			$scope.queue = enrichQueue(data.Queue);
+			$scope.self = data.Self;
+			$scope.playerStatus = data.PlayerStatus;
+		});
+	};
 
-            $scope.getPlayList = function() {
-                var message = {
-                    'Filter': $scope.filter,
-                    'Page': digitize($scope.currentPage) || 1,
-                    'SortBy' : $scope.sortBy,
-                    'Ascending' : $scope.ascending };
+	$scope.getPlayList = function () {
+		var message = {
+			'Filter': $scope.filter,
+			'Page': digitize($scope.currentPage) || 1,
+			'SortBy': $scope.sortBy,
+			'Ascending': $scope.ascending
+		};
 
-                $http({ method: 'POST', url: '/dynamic/playlist', data: message }).success(function (data) {
-                    var currentPage = Math.max(data.CurrentPage, 1);
-                    var totalPageCount = Math.max(data.TotalPageCount, 1);
-                    $scope.sortBy = data.SortBy,
-                    $scope.ascending = data.Ascending;
-                    $scope.searchResult = data.SongsForCurrentPage;
-                    $scope.currentPage = data.CurrentPage;
-                    $scope.pageCount = Math.max(data.TotalPageCount, 1);
-                });
-            };
+		$http({ method: 'POST', url: '/dynamic/playlist', data: message }).success(function (data) {
+			var currentPage = Math.max(data.CurrentPage, 1);
+			var totalPageCount = Math.max(data.TotalPageCount, 1);
+			$scope.sortBy = data.SortBy,
+			$scope.ascending = data.Ascending;
+			$scope.searchResult = data.SongsForCurrentPage;
+			$scope.currentPage = data.CurrentPage;
+			$scope.pageCount = Math.max(data.TotalPageCount, 1);
+		});
+	};
 
-            $scope.getSongName = function (song) {
-                if (song.Artist && song.Name)
-                    return song.Artist + ' - ' + song.Name;
-                else
-                    return song.Artist || song.Name || 'Unknown';
-            };
+	$scope.getSongArtistAndName = function (song) {
+		return $scope.getSongArtist(song) + ' - ' + $scope.getSongName(song);
+	};
 
-            $scope.getCurrentSongName = function () {
-                if (!$scope.playerStatus)
-                    return '';
+	$scope.getSongArtist = function (song) {
+		if (song.Artist)
+			return song.Artist;
 
-                if (!$scope.playerStatus.RequestedSong)
-                    return '';
+		return 'Unknown';
+	};
 
-                var requestedSong = $scope.playerStatus.RequestedSong.Song;
+	$scope.getSongName = function (song) {
+		if (song.Name)
+			return song.Name;
 
-                return $scope.getSongName(requestedSong);
-            };
+		return 'Unknown';
+	};
 
-            $scope.getCurrentSongPosition = function () {
-                if (!$scope.playerStatus)
-                    return convertNumberToTime(0);
+	$scope.getCurrentSongName = function () {
+		if (!$scope.playerStatus)
+			return '';
 
-                return convertNumberToTime($scope.playerStatus.Position || 0);
-            };
+		if (!$scope.playerStatus.RequestedSong)
+			return '';
 
-            $scope.getCurrentSongDuration = function () {
-                if (!$scope.playerStatus)
-                    return convertNumberToTime(0);
+		var requestedSong = $scope.playerStatus.RequestedSong.Song;
 
-                if (!$scope.playerStatus.RequestedSong)
-                    return convertNumberToTime(0);
+		return $scope.getSongArtistAndName(requestedSong);
+	};
 
-                return convertNumberToTime($scope.playerStatus.RequestedSong.Song.Duration);
-            };
+	$scope.getCurrentSongPosition = function () {
+		if (!$scope.playerStatus)
+			return convertNumberToTime(0);
 
-            $scope.getCurrentSongRequester = function () {
-                if (!$scope.playerStatus)
-                    return this.convertNumberToTime();
+		return convertNumberToTime($scope.playerStatus.Position || 0);
+	};
 
-                if (!$scope.playerStatus.RequestedSong)
-                    return '';
+	$scope.getCurrentSongDuration = function () {
+		if (!$scope.playerStatus)
+			return convertNumberToTime(0);
 
-                return $scope.playerStatus.RequestedSong.RequesterName;
-            };
+		if (!$scope.playerStatus.RequestedSong)
+			return convertNumberToTime(0);
 
-            $scope.getVolume = function(){
-                if (!$scope.playerStatus)
-                    return '0';
+		return convertNumberToTime($scope.playerStatus.RequestedSong.Song.Duration);
+	};
 
-                return $scope.playerStatus.Volume;
-            }
+	$scope.getCurrentSongRequester = function () {
+		if (!$scope.playerStatus)
+			return this.convertNumberToTime();
 
-            $scope.getQueueDuration = function () {
-                return convertNumberToTime(
-                    _.chain($scope.queue)
-                    .map(function (requestedSong) { return requestedSong.Song.Duration; })
-                    .reduce(function (state, value) { return state + value; }, 0)
-                    .value());
-            };
+		if (!$scope.playerStatus.RequestedSong)
+			return '';
 
-            $scope.getQueueItemClass = function (item, index) {
-                return 'qitem ' +
-                    (item.RequesterName === $scope.self ? 'self' : '') +
-                    (index % 2 === 0 ? 'even' : 'odd') +
-                    'queueitemcontainer';
-            };
+		return $scope.playerStatus.RequestedSong.RequesterName;
+	};
 
-            $scope.changeVolume = function () {
-                var newVolume = prompt('New volume', String($scope.playerStatus.Volume));
-                if (newVolume) {
-                    $http({ method: 'POST', url: '/dynamic/volume', data: newVolume }).success(function (volume) {
-                        $scope.playerStatus.Volume = volume;
-                    });
-                }
-            };
+	$scope.getVolume = function () {
+		if (!$scope.playerStatus)
+			return '0';
 
-            $scope.next = function () {
-                $http({ method: 'GET', url: '/dynamic/next' });
-            };
+		return $scope.playerStatus.Volume;
+	}
 
-            $scope.pause = function () {
-                $http({ method: 'GET', url: '/dynamic/pause' });
-            };
+	$scope.getQueueDuration = function () {
+		return convertNumberToTime(
+			_.chain($scope.queue)
+			.map(function (requestedSong) { return requestedSong.Song.Duration; })
+			.reduce(function (state, value) { return state + value; }, 0)
+			.value());
+	};
 
-            $scope.rescan = function () {
-                $http({ method: 'GET', url: '/dynamic/rescan' });
-            };
+	$scope.getQueueItemClass = function (item, index) {
+		return 'qitem ' +
+			(item.RequesterName === $scope.self ? 'self' : '') +
+			(index % 2 === 0 ? 'even' : 'odd') +
+			'queueitemcontainer';
+	};
 
-            $scope.addToQueue = function (song) {
-                $http({ method: 'POST', url: '/dynamic/queue', data: song.TempId }).success(function () {
-                    refreshQueue();
-                });
-            };
+	$scope.changeVolume = function () {
+		var newVolume = prompt('New volume', String($scope.playerStatus.Volume));
+		if (newVolume) {
+			$http({ method: 'POST', url: '/dynamic/volume', data: newVolume }).success(function (volume) {
+				$scope.playerStatus.Volume = volume;
+			});
+		}
+	};
 
-            $scope.removeFromQueue = function (song) {
-                $http({ method: 'DELETE', url: '/dynamic/queue', data: song.TempId }).success(function () {
-                    refreshQueue();
-                });
-            };
+	$scope.next = function () {
+		$http({ method: 'GET', url: '/dynamic/next' });
+	};
 
-            $scope.search = function() {
-                $scope.currentPage = 1;
-                $scope.getPlayList();
-            };
+	$scope.pause = function () {
+		$http({ method: 'GET', url: '/dynamic/pause' });
+	};
 
-            $scope.clear = function() {
-                $scope.filter = '';
-                $scope.currentPage = 1;
-                $scope.getPlayList();
-            };
+	$scope.rescan = function () {
+		$http({ method: 'GET', url: '/dynamic/rescan' });
+	};
 
-            $scope.previousPage = function () {
-                $scope.currentPage--;
-                $scope.getPlayList();
-            };
+	$scope.addToQueue = function (song) {
+		$http({ method: 'POST', url: '/dynamic/queue', data: song.TempId }).success(function () {
+			refreshQueue();
+		});
+	};
 
-            $scope.nextPage = function () {
-                $scope.currentPage++;
-                $scope.getPlayList();
-            };
+	$scope.removeFromQueue = function (song) {
+		$http({ method: 'DELETE', url: '/dynamic/queue', data: song.TempId }).success(function () {
+			refreshQueue();
+		});
+	};
 
-            $scope.getHeaderClass = function (headerName) {
-                if (headerName === $scope.sortBy)
-                    return 'sorted' + ($scope.ascending ? ' Asc' : ' Desc');
-                return '';
-            };
+	$scope.search = function () {
+		$scope.currentPage = 1;
+		$scope.getPlayList();
+	};
 
-            $scope.sort = function (sortBy) {
-                $scope.ascending = !$scope.ascending;
-                $scope.sortBy = sortBy;
-                $scope.getPlayList();
-            };
+	$scope.clear = function () {
+		$scope.filter = '';
+		$scope.currentPage = 1;
+		$scope.getPlayList();
+	};
 
-            $scope.getPlaylistWidth = function () {
-                return Math.max(670, $(window).width() - $('#queue').width() - 50);
-            }
-            $scope.$watch($scope.getPlaylistWidth, function (newValue, oldValue) {
-                $scope.playlistStyle = { width: String(newValue) + 'px' };
-            });
-            $(window).on('resize', function () {
-                $scope.$apply();
-            });
+	$scope.previousPage = function () {
+		$scope.currentPage--;
+		$scope.getPlayList();
+	};
+
+	$scope.nextPage = function () {
+		$scope.currentPage++;
+		$scope.getPlayList();
+	};
+
+	$scope.getHeaderClass = function (headerName) {
+		if (headerName === $scope.sortBy)
+			return 'sorted' + ($scope.ascending ? ' Asc' : ' Desc');
+		return '';
+	};
+
+	$scope.sort = function (sortBy) {
+		$scope.ascending = !$scope.ascending;
+		$scope.sortBy = sortBy;
+		$scope.getPlayList();
+	};
 
             refreshQueue();
             $scope.getPlayList();
 
-            (function () {
-                function poll() {
-                    refreshQueue();
-                    $timeout(poll, 1000);
-                };
-                poll();
-            })();
+	(function () {
+		function poll() {
+			refreshQueue();
+			$timeout(poll, 1000);
+		};
+		poll();
+	})();
         })
 })();
