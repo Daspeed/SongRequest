03ae095bb94816a610ce80aa7db3da28eedb879f
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-12-15 03:55:12 -0500

    wrapping up - utils project and fixing all references

--- SongRequest.SongPlayer/SongLibrary.cs
+++ SongRequest.SongPlayer/SongLibrary.cs
@@ -6,6 +6,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using SongRequest.Utils;
 
 namespace SongRequest
 {
